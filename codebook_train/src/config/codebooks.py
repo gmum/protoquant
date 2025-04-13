67194ffd90d07ce4af85802cc5cd8fd6cdfb79fb
Mikolaj <bubus258@gmail.com>
2025-04-13 16:16:22 +0200

    [feat] update vector quantize config to be more explicit

--- codebook_train/src/config/codebooks.py
+++ codebook_train/src/config/codebooks.py
@@ -39,6 +39,25 @@ class VectorQuantizeCodebookConfig(BaseCodebookConfig):
     _target_: str = (
         f"{VectorQuantizeCodebook.__module__}.{VectorQuantizeCodebook.__qualname__}"
     )
+    # https://github.com/lucidrains/vector-quantize-pytorch/blob/8bb740e8dfcb6cd2f76648f971a211dd0d8da766/vector_quantize_pytorch/vector_quantize_pytorch.py#L846
+
+    learnable_codebook: bool = True
+    ema_update: bool = False
+
+    # general
+    decay: float = 0.8
+    eps: float = 1e-5
+    use_cosine_sim: bool = False
+
+    # initialization
+    heads: int = 1
+    kmeans_init: bool = False
+    kmeans_iters: int = 10
+
+    # losses
+    commitment_weight: float = 1.0
+    orthogonal_reg_weight: float = 0.0
+    codebook_diversity_loss_weight: float = 0.0
 
 
 config_store = ConfigStore.instance()
